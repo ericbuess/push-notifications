b39447c1756390c4132830831bb77a1de2c86c51
ericbuess <ericbuess@gmail.com>
2020-08-12 01:08:21 +0000

    Add test config for docker

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ Reads from stdin
 '''
 import requests, json, os
 
-PUSHCUT_SECRET = os.getenv('PUSHCUT_SECRET')
+##PUSHCUT_SECRET = os.getenv('PUSHCUT_SECRET')
 # Take command line parameter but do surround by single quotes 
-
+print('hi')
 #myString="".join(sys.stdin.readlines())
 
-results={'title':'Covid 19','text':'Bell: 600\nSmith: 300','input':[{'bell':{'total':'600'}}]}
+##results={'title':'Covid 19','text':'Bell: 600\nSmith: 300','input':[{'bell':{'total':'600'}}]}
 #results=json.dumps(results)
-r=requests.post("https://api.pushcut.io/" + PUSHCUT_SECRET + "/notifications/covid",json=results,timeout=10)
+##r=requests.post("https://api.pushcut.io/" + PUSHCUT_SECRET + "/notifications/covid",json=results,timeout=10)
 
-print('covid notification result: ', r.text)
+#print('covid notification result: ', r.text)
